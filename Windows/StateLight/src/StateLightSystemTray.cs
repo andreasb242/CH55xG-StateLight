c2df0ce7f80430e338806c15d4532d187746c9d9
Andreas Butti <andreasbutti@gmail.com>
2019-05-11 21:33:14 +0200

    Code Cleanup

--- Windows/StateLight/src/StateLightSystemTray.cs
+++ Windows/StateLight/src/StateLightSystemTray.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StateLight.src;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
@@ -84,66 +85,6 @@ namespace StateLight
 			trayIcon.Visible = false;
 
 			Application.Exit();
-		}
-	}
-
-
-	class ColorMenuItem : MenuItem
-	{
-		/// <summary>
-		/// Menu Item Name
-		/// </summary>
-		private String name;
-
-		/// <summary>
-		/// Color
-		/// </summary>
-		private Color color;
-
-		/// <summary>
-		/// LED Connection to the hardware
-		/// </summary>
-		private LedConnection led;
-
-		private const int MENU_HEIGHT = 22;
-		private const int MENU_WIDTH = 150;
-
-		public ColorMenuItem(LedConnection led, string name, Color color) : base(name)
-		{
-			this.led = led;
-			this.name = name;
-			this.color = color;
-
-			OwnerDraw = true;
-			Click += new EventHandler(MenuHelpOnClick);
-			DrawItem += new DrawItemEventHandler(MenuHelpOnDrawItem);
-			MeasureItem += new MeasureItemEventHandler(MenuHelpOnMeasureItem);
-		}
-
-		void MenuHelpOnMeasureItem(object obj, MeasureItemEventArgs miea)
-		{
-			miea.ItemWidth = MENU_WIDTH;
-			miea.ItemHeight = MENU_HEIGHT;
-		}
-
-		void MenuHelpOnDrawItem(object obj, DrawItemEventArgs e)
-		{
-			Graphics g = e.Graphics;
-			e.DrawBackground();
-			Rectangle bounds = e.Bounds;
-
-			Brush foreground = new SolidBrush(e.ForeColor);
-			Rectangle r = new Rectangle(bounds.Left + 4, bounds.Top + 4, 8, 8);
-			g.FillRectangle(new SolidBrush(color), r);
-			g.DrawRectangle(new Pen(foreground), r);
-			g.DrawString(name, e.Font, foreground, new PointF(bounds.Left + 20, bounds.Top));
-		}
-
-		void MenuHelpOnClick(object obj, EventArgs ea)
-		{
-			int factor = Properties.Settings.Default.LedBrightness;
-			int color = (this.color.R / factor) << 16 | (this.color.G / factor) << 8 | this.color.B / factor;
-			led.WriteColor(color);
 		}
 	}
 }
