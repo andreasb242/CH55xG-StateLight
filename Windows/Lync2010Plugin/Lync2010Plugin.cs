38b521e90ffe894f11bba3b3da61cd99b4b4e0f7
Andreas Butti <andreas.butti@gmail.com>
2019-05-21 20:09:12 +0200

    Connecting Lync2010 working

---
+++ Windows/Lync2010Plugin/Lync2010Plugin.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Lync.Model;
+using StateLightPluginDef;
+
+namespace Lync2010Plugin
+{
+    public class Lync2010PluginImpl : IStateLightPluginDef
+    {
+        /// <summary>
+        /// Client, set in constructor if found
+        /// </summary>
+        private LyncClient lyncClient = null;
+
+        /// <summary>
+        /// Flag to enable event forwarding
+        /// </summary>
+        private bool sendEvent = false;
+
+        /// <summary>
+        /// Callback handler
+        /// </summary>
+        private IStateProvider callback;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Lync2010PluginImpl()
+        {
+            ConnectLyncClient();
+        }
+
+        /// <summary>
+        /// Connect to the lync client, if possible
+        /// </summary>
+        private void ConnectLyncClient()
+        {
+            Console.WriteLine("Connecting Lync...");
+
+            try
+            {
+                lyncClient = LyncClient.GetClient();
+                lyncClient.StateChanged += CallbackStateChanged;
+
+                if (lyncClient.State == ClientState.SignedIn)
+                {
+                    lyncClient.Self.Contact.ContactInformationChanged += CallbackContactInformationChanged;
+                }
+            }
+            catch (ClientNotFoundException)
+            {
+                Console.WriteLine("Lync not running");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception connection lync: " + e.ToString());
+            }
+        }
+
+
+        public bool IsSupported()
+        {
+            return lyncClient != null;
+        }
+
+        public string GetDetails()
+        {
+            return "Lync 2010 Plugin, Andreas Butti, 2019";
+        }
+
+        public string DisplayName()
+        {
+            return "Lync 2010";
+        }
+
+        public void Start(IStateProvider state)
+        {
+            callback = state;
+            sendEvent = true;
+        }
+
+        public void Stop()
+        {
+            sendEvent = false;
+        }
+
+        /// <summary>
+        /// Callback, called by lync
+        /// </summary>
+        /// <param name="sender">Sender</param>
+        /// <param name="e">ContactInformationChangedEventArgs</param>
+        void CallbackContactInformationChanged(object sender, ContactInformationChangedEventArgs e)
+        {
+            if (!sendEvent)
+            {
+                return;
+            }
+
+            if (!e.ChangedContactInformation.Contains(ContactInformationType.Availability))
+            {
+                return;
+            }
+
+            SendState();
+        }
+
+
+        /// <summary>
+        /// Send state to callback
+        /// </summary>
+        private void SendState()
+        {
+            if (callback == null)
+            {
+                return;
+            }
+
+            if (lyncClient.State != ClientState.SignedIn)
+            {
+                callback.WriteState("signed-out", "");
+                return;
+            }
+
+
+            ContactAvailability currentAvailability = (ContactAvailability)lyncClient.Self.Contact.GetContactInformation(ContactInformationType.Availability);
+
+            // Busy, BusyIdle, Free, FreeIdle, Away, TemporarilyAway, DoNotDisturb, Offline
+            callback.WriteState(currentAvailability.ToString(), "");
+        }
+
+        void CallbackStateChanged(object sender, ClientStateChangedEventArgs e)
+        {
+            switch (e.NewState)
+            {
+                case ClientState.Initializing:
+                    break;
+
+                case ClientState.Invalid:
+                    break;
+
+                case ClientState.ShuttingDown:
+                    break;
+
+                case ClientState.SignedIn:
+                    lyncClient.Self.Contact.ContactInformationChanged += CallbackContactInformationChanged;
+                    SendState();
+                    break;
+
+                case ClientState.SignedOut:
+                    if (sendEvent)
+                    {
+                        callback.WriteState("signed-out", "");
+                    }
+                    lyncClient.StateChanged -= CallbackStateChanged;
+                    break;
+
+                case ClientState.SigningIn:
+                    break;
+
+                case ClientState.SigningOut:
+                    break;
+
+                case ClientState.Uninitialized:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
--- Windows/StateLight/app/Plugins.cs
+++ Windows/StateLight/app/Plugins.cs
@@ -37,24 +37,39 @@ namespace StateLight.app
 		public void LoadPluginList()
 		{
 			String path = Application.StartupPath + "\\plugin";
-			string[] pluginFiles = Directory.GetFiles(path, "*.dll");
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Plugin folder \"" + path + "\" does not exist");
+                return;
+            }
 
-			IStateLightPluginDef[] pluginList = (
-				// From each file in the files.
-				from file in pluginFiles
-					// Load the assembly.
-				let asm = Assembly.LoadFile(file)
-				// For every type in the assembly that is visible outside of
-				// the assembly.
-				from type in asm.GetExportedTypes()
-					// Where the type implements the interface.
-				where typeof(IStateLightPluginDef).IsAssignableFrom(type)
-				// Create the instance.
-				select (IStateLightPluginDef)Activator.CreateInstance(type)
-			// Materialize to an array.
-			).ToArray();
+			string[] pluginFiles = Directory.GetFiles(path, "*Plugin.dll");
 
-			foreach (IStateLightPluginDef p in pluginList)
+            IStateLightPluginDef[] pluginList = { };
+
+            try
+            {
+                pluginList = (
+                    // From each file in the files.
+                    from file in pluginFiles
+                        // Load the assembly.
+                let asm = Assembly.LoadFile(file)
+                // For every type in the assembly that is visible outside of
+                // the assembly.
+                from type in asm.GetExportedTypes()
+                    // Where the type implements the interface.
+                where typeof(IStateLightPluginDef).IsAssignableFrom(type)
+                // Create the instance.
+                select (IStateLightPluginDef)Activator.CreateInstance(type)
+                // Materialize to an array.
+                ).ToArray();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception loading plugin: " + e.ToString());
+            }
+
+            foreach (IStateLightPluginDef p in pluginList)
 			{
 				Console.WriteLine("Load Plugin: \"" + p.GetDetails() + "\", supported: " + p.IsSupported());
 
